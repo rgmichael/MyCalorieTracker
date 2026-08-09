216798988959045f368731ecd25a460d26a5d5d8
Michael <rgmichael@gmail.com>
2026-08-08 22:20:56 -0300

    setup part 1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,4 +24,11 @@ dependencyResolutionManagement {
 
 rootProject.name = "MyCaloryTracker"
 include(":app")
- 
+include(":core")
+include(":onboarding")
+include(":onboarding:onboarding_presentation")
+include(":onboarding:onboarding_domain")
+include(":tracker")
+include(":tracker:tracker_data")
+include(":tracker:tracker_domain")
+include(":tracker:tracker_presentation")
