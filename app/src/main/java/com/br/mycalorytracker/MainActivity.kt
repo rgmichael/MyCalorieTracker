c9994b60a3961474562bf7753053c6af60acc577
Michael <rgmichael@gmail.com>
2026-08-29 22:21:21 -0300

    implementing the height and weight screens and viewmodels

--- app/src/main/java/com/br/mycalorytracker/MainActivity.kt
+++ app/src/main/java/com/br/mycalorytracker/MainActivity.kt
@@ -6,9 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -17,6 +15,8 @@ import com.br.core.navigation.Route
 import com.br.mycalorytracker.ui.theme.MyCaloryTrackerTheme
 import com.br.onboarding_presentation.age.AgeScreen
 import com.br.onboarding_presentation.gender.GenderScreen
+import com.br.onboarding_presentation.height.HeightScreen
+import com.br.onboarding_presentation.weight.WeightScreen
 import com.br.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -50,10 +50,16 @@ class MainActivity : ComponentActivity() {
                             GenderScreen(onNavigate = navController::navigate)
                         }
                         composable(Route.HEIGHT) {
-
+                            HeightScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
                         }
                         composable(Route.WEIGHT) {
-
+                            WeightScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
                         }
                         composable(Route.NUTRIENT_GOAL) {
 
