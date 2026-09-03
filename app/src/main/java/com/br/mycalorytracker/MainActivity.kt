e72d6b1695e08dffc7316d01e03a1a8ab570ca8b
Michael <rgmichael@gmail.com>
2026-09-02 22:27:29 -0300

    implementing the goal and activity screens and vm

--- app/src/main/java/com/br/mycalorytracker/MainActivity.kt
+++ app/src/main/java/com/br/mycalorytracker/MainActivity.kt
@@ -13,8 +13,10 @@ import androidx.navigation.compose.rememberNavController
 import com.br.mycalorytracker.navigation.navigate
 import com.br.core.navigation.Route
 import com.br.mycalorytracker.ui.theme.MyCaloryTrackerTheme
+import com.br.onboarding_presentation.activity.ActivityScreen
 import com.br.onboarding_presentation.age.AgeScreen
 import com.br.onboarding_presentation.gender.GenderScreen
+import com.br.onboarding_presentation.goal.GoalScreen
 import com.br.onboarding_presentation.height.HeightScreen
 import com.br.onboarding_presentation.weight.WeightScreen
 import com.br.onboarding_presentation.welcome.WelcomeScreen
@@ -65,10 +67,10 @@ class MainActivity : ComponentActivity() {
 
                         }
                         composable(Route.ACTIVITY) {
-
+                            ActivityScreen(onNavigate = navController::navigate)
                         }
                         composable(Route.GOAL) {
-
+                            GoalScreen(onNavigate = navController::navigate)
                         }
 
                         composable(Route.TRACKER_OVERVIEW) {
