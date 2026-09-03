0b929288dd104c6ee5f1fcbda816e09ae6a70a13
Michael <rgmichael@gmail.com>
2026-09-02 23:05:19 -0300

    implementing the nutrient goal components

--- app/src/main/java/com/br/mycalorytracker/MainActivity.kt
+++ app/src/main/java/com/br/mycalorytracker/MainActivity.kt
@@ -18,6 +18,7 @@ import com.br.onboarding_presentation.age.AgeScreen
 import com.br.onboarding_presentation.gender.GenderScreen
 import com.br.onboarding_presentation.goal.GoalScreen
 import com.br.onboarding_presentation.height.HeightScreen
+import com.br.onboarding_presentation.nutrient_goal.NutrientGoalScreen
 import com.br.onboarding_presentation.weight.WeightScreen
 import com.br.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -64,7 +65,10 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                         composable(Route.NUTRIENT_GOAL) {
-
+                            NutrientGoalScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
                         }
                         composable(Route.ACTIVITY) {
                             ActivityScreen(onNavigate = navController::navigate)
